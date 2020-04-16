3b29e0f537e011e37377c555bb92f9df18390854
hyunnna <wjdgusghk904@naver.com>
2020-04-16 21:25:59 +0900

    Modify

--- src/com/vend/service/ChangeService.java
+++ src/com/vend/service/ChangeService.java
@@ -1,5 +1,21 @@
+
 package com.vend.service;
 
 public class ChangeService {
-    // ddsda
+    static final int ITEM_PRICE_1 = 3000;
+    static final int ITEM_PRICE_2 = 5000;
+    static final int ITEM_PRICE_3 = 1000;
+
+    public void change(int item_number_1,int item_number_2,int item_number_3,int put_money){
+
+        int d;
+        int c;
+
+        d = item_number_1 * ITEM_PRICE_1 + item_number_2*ITEM_PRICE_2 + item_number_3*ITEM_PRICE_3;
+        c = put_money - d;
+
+        System.out.println("총금액 : " + d);
+        System.out.println("잔돈은 : " + c);
+
+    }
 }
--- src/com/vend/vendingmachine/Main.java
+++ src/com/vend/vendingmachine/Main.java
@@ -1,9 +1,12 @@
 package com.vend.vendingmachine;
 
+import com.vend.service.ChangeService;
 import com.vend.view.VendingMachineView;
 
 public class Main {
-    public staticx   void main(String[] args) {
-        new VendingMachineView();
+    public static void main(String[] args) {
+        //new VendingMachineView();
+        new ChangeService().change(1,2,4,500000);
     }
+
 }
