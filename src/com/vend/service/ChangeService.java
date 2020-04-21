6bbfef45943ed32a478ad4253b730ebe4838f0db
Your Name <201701704@inu.ac.kr>
2020-04-22 01:45:13 +0900

    mid assignment

--- src/com/vend/service/ChangeService.java
+++ src/com/vend/service/ChangeService.java
@@ -2,61 +2,54 @@ package com.vend.service;
 import java.util.Scanner;
 
 public class ChangeService {
-    static final int ITEM_PRICE_1 = 3000;
-    static final int ITEM_PRICE_2 = 4000;
-    static final int ITEM_PRICE_3 = 1800;
 
-    public static void main() {
+    public ChangeService(String a,String b) {
+        String [] Total_price=a.split("원");
+        int ITEM_PRICE_1 = 3000;
+        int ITEM_PRICE_2 = 4000;
+        int ITEM_PRICE_3 = 1800;
         Scanner sc = new Scanner(System.in);
+        int Int_total_price = Integer.parseInt(Total_price[0]);
+        int money = Integer.parseInt(b);
+
         //물건 수량은 랜덤으로 정의
         int first_item = (int)(Math.random()*10)+1;
         int second_item = (int)(Math.random()*10)+1;
         int third_item = (int)(Math.random()*10)+1;
 
-        int all_price = (first_item * ITEM_PRICE_1) + (second_item * ITEM_PRICE_2) + (third_item * ITEM_PRICE_3);
+        for(int i=0; i<2;i++) {
+            int D_A = money - Int_total_price;
+            while (D_A < 0) {
+                //TODO 소지금이 총비용을 넘어가는 경우를 입력하면 멈춤.
+                System.out.println("투입금액이 적습니다. 돈을 더 넣어주세요.");
+                money = sc.nextInt();
+            }
 
-        System.out.println("지불할금액을 입력하세요 : ");
-        int put_money = sc.nextInt();
+            int Cmoney[][] = new int[][]{
+                    {10000, 5000, 1000, 500, 100, 10, 1},      //일반적인 상황
+                    {10000, 5000, 1200, 1000, 500, 100, 10, 1} //그리디??
+            };
 
-        int D_A;
-        D_A = put_money - all_price;
-        while(D_A<0){
-            System.out.println("투입금액이 적습니다. 돈을 더 넣어주세요.");
-            put_money=sc.nextInt();
+            int coin_num = 0;
+            int bill_num = 0;
+            for (int j = 0; j < Cmoney[i].length; j++) {
+                if (D_A >= 1000) {
+                    bill_num += D_A / Cmoney[i][j];
+                    D_A = D_A % Cmoney[i][j];
+                }
+
+                if (D_A < 1000) {
+                    coin_num += D_A / Cmoney[i][j];
+                    D_A = D_A % Cmoney[i][j];
+                }
+            }
+            if(i==1)
+                System.out.println("1200원짜리 지폐가 있다고하면");
+            System.out.println("지폐 갯수는" + bill_num + "장이며");
+            System.out.println("동전 갯수는" + coin_num + "개 입니다.");
+            System.out.println("");
+
+            sc.close();
         }
-
-        int coin[]= new int[]{10000, 5000, 1000, 500, 100, 10, 1};
-        int coin_[]= new int[]{10000, 5000, 1200, 1000, 500, 100, 10, 1};
-        int coin_num=0;
-        int bill_num=0;
-        int rest_m=0;
-        for(int i=0; i<coin.length;i++){
-            if(rest_m>1000){
-                bill_num += D_A/coin[i];
-                rest_m=D_A%coin[i];
-            }
-
-            if(rest_m<1000){
-                coin_num += D_A/coin[i];
-                rest_m=D_A%coin[i];
-            }
-        }
-        System.out.println("지폐 갯수는"+bill_num +"이며");
-        System.out.println("동전 갯수는"+coin_num +"입니다.");
-        for(int i=0; i<coin_.length;i++){
-            if(rest_m>1000){
-                bill_num += D_A/coin_[i];
-                rest_m=D_A%coin_[i];
-            }
-
-            if(rest_m<1000){
-                coin_num += D_A/coin_[i];
-                rest_m=D_A%coin_[i];
-            }
-        }
-        System.out.println("1200원짜리 지폐가 있다고 가정할 경우");
-        System.out.println("지폐 갯수는"+bill_num +"이며");
-        System.out.println("동전 갯수는"+coin_num +"입니다.");
-        sc.close();
     }
 }
--- src/com/vend/vendingmachine/Main.java
+++ src/com/vend/vendingmachine/Main.java
@@ -5,7 +5,7 @@ import com.vend.view.VendingMachineView;
 
 public class Main {
     public static void main(String[] args) {
-        //new VendingMachineView();
+        new VendingMachineView();
 
     }
 }
